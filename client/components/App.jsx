9b665e1418227f4759c02eab3067390aa7052695
ErinLlouise <erinllouise3@gmail.com>
2021-04-09 07:30:32 +1200

    add functions to App and Home

---
+++ client/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Home from './Home'
+import Landmarks from './Landmarks'
+import Regions from './Regions'
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  )
+}
+
+export default App
