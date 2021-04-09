db2227acffa457d7c0e70831ca3c907f942aab99
root <root@Ignis-PC.localdomain>
2021-04-09 12:07:08 +1200

    home.jsx fucntioning

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
+const homeImage = "/images/LGPE_Walking_Pokémon.png"
+
 function Home() {
   return (
-    <>
-      <h1>THIS IS A TEST</h1>
-    </>
+    <div>
+      <h1>Pokemap</h1>
+      <img src={homeImage} />
+    </div>
   )
 }
 
--- server/public/bundle.js
+++ server/public/bundle.js
@@ -41,9 +41,12 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 
+var homeImage = "/images/LGPE_Walking_Pokémon.png";
 
 function Home() {
-  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(react__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h1", null, "THIS IS A TEST"));
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h1", null, "Pokemap"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("img", {
+    src: homeImage
+  }));
 }
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Home);
